a54a529567b5fd1d7c489d970684f8d23a9424ef
rpolicastro <rpolicas@indiana.edu>
2020-07-12 11:23:32 -0400

    modularizing shiny code

--- inst/myapp/ui.R
+++ inst/myapp/ui.R
@@ -1,25 +1,3 @@
 
-# Define UI for miles per gallon app ----
-ui <- pageWithSidebar(
-  # App title ----
-  headerPanel("Miles Per Gallon"),
-
-  # Sidebar panel for inputs ----
-  sidebarPanel(
-    # Input: Selector for variable to plot against mpg ----
-    selectInput(
-      "variable", "Variable:", 
-      c("Cylinders" = "cyl", "Transmission" = "am","Gears" = "gear")
-    ),
-    # Input: Checkbox for whether outliers should be included ----
-    checkboxInput("outliers", "Show outliers", TRUE)
-  ),
-
-  # Main panel for displaying outputs ----
-  mainPanel(
-    # Output: Formatted text for caption ----
-    h3(textOutput("caption")),
-    # Output: Plot of the requested variable against mpg ----
-    plotOutput("mpgPlot")
-  )
-)
+# Define UI for miles per gallon app
+ui <- mpgUI("mpg")
